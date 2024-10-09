be5f13c9cf0757f98ebfcafcf24fa7dddbe1f4fa
Brian Santos <142011551+briansantoss@users.noreply.github.com>
2024-10-09 13:21:42 -0300

    Refatorando código do endpoint de inserção

--- CustomTasks/Program.cs
+++ CustomTasks/Program.cs
@@ -2,19 +2,17 @@ using CustomTasks.Models;
 using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDataContext>();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
 // Endpoint para a funcionalidade de criação e inserção de tarefas
-app.MapPost("/tasks/create", ([FromBody] CustomTasks.Models.Task task) => 
+app.MapPost("/tasks/create", ([FromBody] CustomTasks.Models.Task task, [FromServices] AppDataContext context) => 
 {
-    using(var context = new AppDataContext())
-    {
-        context.Add(task);
-        context.SaveChanges();
-        return Results.Created("", task);
-    }
+    context.Tasks.Add(task);
+    context.SaveChanges();
+    return Results.Created("", task);
 });
 
 app.Run();
