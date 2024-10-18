562e773f47826aba9d605ab287747eccfb4b2818
Brian Santos <142011551+briansantoss@users.noreply.github.com>
2024-10-18 14:02:11 -0300

    Atualizando tabela de rótulos

--- CustomTasks/Models/Label.cs
+++ CustomTasks/Models/Label.cs
@@ -1,11 +1,9 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace CustomTasks.Models;
 
 public class Label
 {
     public int LabelId { get; set; }
-    [Required] public string LabelName { get; set; } = null!;
+    public string LabelName { get; set; } = null!;
 
 /* Configurações de relacionamentos entre usuários (tabela "Users" e rótulos (tabela "Labels")
     public int? UserId { get; set; }
