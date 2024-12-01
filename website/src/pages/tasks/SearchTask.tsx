09f4fedf4cd6bb8f7d2345bc649b9a137089b3a9
felipe <velharvafelipe@gmail.com>
2024-12-01 19:54:44 -0300

    mostrar os rótulos disponíveis na lista

--- website/src/pages/tasks/SearchTask.tsx
+++ website/src/pages/tasks/SearchTask.tsx
@@ -4,93 +4,85 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 
 function SearchTask() {
-    const [tasks, setUsers] = useState<Task[]>([]);
-    const { username } = useParams<{ username: string }>();
-    useEffect(() =>{
-        fetch(`http://localhost:5182/tasks/list/${username}`)	
-            .then(resposta => {
-                if (!resposta.ok) {
-                    throw new Error('Erro na resposta do servidor');
-                  }
-                return resposta.json();
-            })
-            .then(tasks => {
-                setUsers(tasks)
-            });
-        
-    });
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const { username } = useParams<{ username: string }>();
 
+  useEffect(() => {
+    fetch(`http://localhost:5182/tasks/list/${username}`)
+      .then((resposta) => {
+        if (!resposta.ok) {
+          throw new Error("Erro na resposta do servidor");
+        }
+        return resposta.json();
+      })
+      .then((tasks) => {
+        setTasks(tasks);
+      });
+  }, [username]);
 
-    function status(taskId: number) {
-        axios
-          .put(`http://localhost:5182/tasks/status/${taskId}`)
-          console.log(`Id: ${taskId} completed` );
-      }
+  function status(taskId: number) {
+    axios.put(`http://localhost:5182/tasks/status/${taskId}`);
+    console.log(`Id: ${taskId} completed`);
+  }
 
-    function remove(taskId: number) {
-        axios
-          .delete(`http://localhost:5182/tasks/delete/${taskId}`)
-      }
+  function remove(taskId: number) {
+    axios.delete(`http://localhost:5182/tasks/delete/${taskId}`);
+  }
 
-    
-    return (
-        <div>
-        <h1>Task for User - Insira o nome na url</h1>
-        <table >
-            <thead>
-                <tr>
-                    <th>Titulo</th>
-                    <th>Descrição</th>
-                    <th>Completada</th>
-                    <th>Data de Crianção</th>
-                    <th>Status</th>
-                    <th>Deletar Task</th>
-                </tr>
-            </thead>
-            <tbody>
-                {tasks.map(task => (
-                    <tr key={task.taskId}>
-                        <td>{task.name}</td>
-                        <td>{task.description}</td>
-                        <td> {task.isCompleted}</td>
-                        <td> {task.createdAt}</td>
-                        <td>
-                            <button
-                            onClick={() => {
-                                status(task.taskId!);
-                            }} >
-                            Update
-                            </button>
-                        </td>
-                        <td>
-                            <button
-                            onClick={() => {
-                                remove(task.taskId!);
-                            }} >
-                            Delete
-                            </button>
-                        </td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
+  return (
+    <div>
+      <h1>Task for User - Insira o nome na URL</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Descrição</th>
+            <th>Completada</th>
+            <th>Data de Criação</th>
+            <th>Rótulos</th>
+            <th>Status</th>
+            <th>Deletar Task</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tasks.map((task) => (
+            <tr key={task.taskId}>
+              <td>{task.name}</td>
+              <td>{task.description}</td>
+              <td>{task.isCompleted ? "Sim" : "Não"}</td>
+              <td>{task.createdAt}</td>
+              <td>
+                {task.labels && task.labels.length > 0
+                  ? task.labels.map((label) => label.labelName).join(", ")
+                  : "Sem rótulos"}
+              </td>
+              <td>
+                <button
+                  onClick={() => {
+                    status(task.taskId!);
+                  }}
+                >
+                  Update
+                </button>
+              </td>
+              <td>
+                <button
+                  onClick={() => {
+                    remove(task.taskId!);
+                  }}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-    
+
 
 
     );
-    
-
-
-
-
-
-
-
-
-
-
-
 }
 
 
